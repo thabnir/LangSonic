02aac97fca51b8307adf61453e2c32187eece53e
Thabnir <thabtheman@gmail.com>
2023-10-23 03:51:13 -0400

    deleted unused old files

--- initialdata.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from torch.utils.data import DataLoader
-
-cv_13 = load_dataset("mozilla-foundation/common_voice_13_0", "hi", split="train")
-dataloader = DataLoader(cv_13, batch_size=32) # DOES NOT WORK! Do we need auth token to access data?
-
-
-languages = ["en","fr","hi","ar","it","es"] # languages to be used 
